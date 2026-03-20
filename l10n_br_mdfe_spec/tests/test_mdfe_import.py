73fac5a9b70d9ed62e3b8a0c34773b4011a411f8
Magno Costa <magno.costa@akretion.com.br>
2026-03-20 03:27:38 +0000

    [FIX] l10n_br_mdfe_spec: adapt test to upstream Odoo change

--- l10n_br_mdfe_spec/tests/test_mdfe_import.py
+++ l10n_br_mdfe_spec/tests/test_mdfe_import.py
@@ -11,6 +11,7 @@ from nfelib.mdfe.bindings.v3_0.mdfe_v3_00 import Tmdfe
 from odoo_test_helper import FakeModelLoader
 
 from odoo import Command, api, models
+from odoo.models import MAGIC_COLUMNS
 from odoo.tests import TransactionCase
 
 from odoo.addons.l10n_br_mdfe_spec.models.v3_0 import mdfe_tipos_basico_v3_00
@@ -18,6 +19,36 @@ from odoo.addons.l10n_br_mdfe_spec.models.v3_0 import mdfe_tipos_basico_v3_00
 from ..models import spec_mixin
 
 tz_datetime = re.compile(r".*[-+]0[0-9]:00$")
+
+
+# Store the original _add_field method
+original_add_field = models.BaseModel._add_field
+
+
+# Define magic fields that should bypass validation
+MAGIC_FIELDS = MAGIC_COLUMNS + ["display_name"]
+
+
+def patched_add_field(self, name, field):
+    """
+    Patched _add_field that allows magic columns for
+    dynamically created test models.
+    """
+    if name in MAGIC_FIELDS:
+        # Allow magic fields without validation
+        cls = self.env.registry[self._name]
+        if not isinstance(getattr(cls, name, field), models.fields.Field):
+            setattr(cls, name, field)
+        field._toplevel = True
+        field.__set_name__(cls, name)
+        cls._fields[name] = field
+    else:
+        # Call original method for other fields
+        original_add_field(self, name, field)
+
+
+# Apply the patch
+models.BaseModel._add_field = patched_add_field
 
 
 @api.model
@@ -114,13 +145,12 @@ spec_mixin.MdfeSpecMixin.build_attrs_fake = build_attrs_fake
 spec_mixin.MdfeSpecMixin.match_or_create_m2o_fake = match_or_create_m2o_fake
 
 
-class NFeImportTest(TransactionCase):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
-        cls.loader = FakeModelLoader(cls.env, cls.__module__)
-        cls.loader.backup_registry()
+class NFeImportTest(TransactionCase, FakeModelLoader):
+    def setUp(self):
+        super().setUp()
+        self.env = self.env(context=dict(self.env.context, tracking_disable=True))
+        self.loader = FakeModelLoader(self.env, self.__module__)
+        self.loader.backup_registry()
 
         # Get all classes from the module that inherit from AbstractModel
         modified_classes = []
@@ -134,12 +164,8 @@ class NFeImportTest(TransactionCase):
 
                 # Replace original class in module
                 modified_classes.append(modified_class)
-                cls.loader.update_registry(modified_classes)
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.loader.restore_registry()
-        super().tearDownClass()
+        self.loader.update_registry(modified_classes)
+        self.addCleanup(self.loader.restore_registry)
 
     def test_import_mdfe1(self):
         file = (
